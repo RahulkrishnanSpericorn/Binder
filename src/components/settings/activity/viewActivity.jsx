40a3faad2d360e6bdcfea6dcab012466fbb69d4d
Krishnanunni <krishnanunni@spericorn.com>
2020-12-04 21:46:59 +0530

    activity view page

--- src/components/settings/activity/viewActivity.jsx
+++ src/components/settings/activity/viewActivity.jsx
@@ -16,26 +16,52 @@ class viewBinder extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            binderItem: {},
+            activityItem: {},
             consultancy_id: null,
-            client_id: null
+            client_id: null,
+            displayData: [
+                { name: "Code", key: "code" },
+                { name: "Activity Type", key: "activity_type" },
+                { name: "Binder", key: "binder.name" },
+                { name: "Client", key: "client.name" },
+                { name: "Consultancy", key: "consultancy.name" },
+                { name: "Display Order", key: "display_order" },
+                { name: "Reference", key: "reference" },
+                { name: "Activity Description", key: "activity_description" },
+                { name: "Activity text", key: "activity_text" },
+                { name: "Activity Tooltip", key: "activity_tooltip" },
+                { name: "Interval Type", key: "interval_type" },
+                { name: "Frequency", key: "frequency" },
+                { name: "Test Frequency", key: "test_frequency" },
+                { name: "Completion Threshold", key: "completion_threshold" },
+                { name: "Email Threshold", key: "email_threshold" },
+                { name: "Code Reference", key: "code_reference" },
+                { name: "Code Reference Tooltip", key: "code_reference_tooltip" },
+                { name: "Quarterly View", key: "quarterly_view" },
+                { name: "Edit form", key: "edit_form" },
+                { name: "Default Total Devices", key: "default_total_devices" },
+                { name: "Start Date", key: "start_date" },
+                { name: "End Date", key: "end_date" },
+                { name: "Created At", key: "created_at" },
+                { name: "Updated At", key: "updated_at" }
+            ]
         };
     }
 
     componentDidMount = () => {
         this.setState({
-            binderItem: this.props.history.location.state.binderItem || {},
+            activityItem: this.props.history.location.state.activityItem || {},
             consultancy_id: this.props.history.location.state.consultancy_id || null,
             client_id: this.props.history.location.state.client_id || null
         });
     };
 
-    backToBinder = () => {
-        history.push("/binder");
+    backToActivity = () => {
+        history.push("/activity");
     };
 
     render() {
-        const { binderItem } = this.state;
+        const { activityItem, displayData } = this.state;
         return (
             <section className="cont-ara">
                 <div className="fst">
@@ -50,7 +76,7 @@ class viewBinder extends Component {
                                 </li>
                                 <li>
                                     <a data-toggle="tab" href="#fls">
-                                        Activity
+                                        Procedure
                                     </a>
                                 </li>
                                 <li>
@@ -63,7 +89,7 @@ class viewBinder extends Component {
                         <div class="pagenation">
                             <ul>
                                 <li>
-                                    <a href="#">Binder </a>
+                                    <a href="#">Activity </a>
                                 </li>
 
                                 <li class="active">
@@ -76,144 +102,25 @@ class viewBinder extends Component {
                         <div className="pub-ara six">
                             <div className="frm-ara three-col">
                                 <div className="frm sav">
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">01</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Binder Name
-                                                </label>
-                                                <h3>{binderItem.name || "-"}</h3>
+                                    {displayData.map((item, i) => (
+                                        <div key={i} className="itm">
+                                            <div className="cunt">
+                                                <div className="numb">{i + 1}</div>
+                                            </div>
+                                            <div className="itm-cnt">
+                                                <div className="form-group">
+                                                    <label className="form-control-placeholder" for="f-name">
+                                                        {item.name}
+                                                    </label>
+                                                    <h3>{activityItem[item.key] || "-"}</h3>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">02</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Display Name
-                                                </label>
-                                                <h3>{binderItem.display_name || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">03</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group select-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Consultancy
-                                                </label>
-                                                <h3>{(binderItem.consultancy && binderItem.consultancy.name) || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">04</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group select-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Client
-                                                </label>
-                                                <h3>{(binderItem.client && binderItem.client.name) || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">05</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group select-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Client User
-                                                </label>
-                                                <h3>-</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">06</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group select-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Consultancy User
-                                                </label>
-                                                <h3>-</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">07</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Color
-                                                </label>
-                                                <h3>{binderItem.color || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">07</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Text Color
-                                                </label>
-                                                <h3>{binderItem.text_color || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">07</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Line
-                                                </label>
-                                                <h3>{binderItem.line || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="itm">
-                                        <div className="cunt">
-                                            <div className="numb">07</div>
-                                        </div>
-                                        <div className="itm-cnt">
-                                            <div className="form-group">
-                                                <label className="form-control-placeholder" for="f-name">
-                                                    Order
-                                                </label>
-                                                <h3>{binderItem.order || "-"}</h3>
-                                            </div>
-                                        </div>
-                                    </div>
+                                    ))}
                                 </div>
 
                                 <div className="frm btn-sec">
-                                    <button onClick={() => this.backToBinder()} className="btn btn-submit">
+                                    <button onClick={() => this.backToActivity()} className="btn btn-submit">
                                         <i className="material-icons tic"> check</i>Go Back{" "}
                                     </button>
                                 </div>
